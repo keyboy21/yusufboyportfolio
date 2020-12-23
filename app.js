30ee8755ed9e70cb0740cb930225237960a9c9ce
yusuf <yusufsaydullayev@gmail.com>
2020-12-23 20:12:03 +0500

    fix6

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(minify());
-app.use(minify({cache: __dirname + '/node_modules'}));
+app.use(minify({ cache: __dirname + '/node_modules' }));
 app.use(minify());
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
