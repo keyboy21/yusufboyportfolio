6cad350c7246419fdc6ba92caeb64ad87c36fef2
yusuf <yusufsaydullayev@gmail.com>
2020-12-23 19:37:04 +0500

    Particle

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const msgRouter = require('./routes/message');
@@ -24,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(express.static(path.join(__dirname, './Works/')));
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
